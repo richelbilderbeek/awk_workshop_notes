fd93305271c40b6912fffc5a6693c4d20bd2b760
richelbilderbeek <richel@richelbilderbeek.nl>
2023-08-25 08:22:33 +0200

    Download data

---
+++ download_data.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+# 4.9 MB                                                      
+wget ftp://ftp.ensembl.org/pub/release-101/gff3/drosophila_melanogaster/Drosophila_melanogaster.BDGP6.28.101.gff3.gz
+                                                              
+# 41 MB                                                       
+wget ftp://ftp.ensemblgenomes.org/pub/metazoa/release-48/fasta/drosophila_melanogaster/dna/Drosophila_melanogaster.BDGP6.28.dna.toplevel.fa.gz
+                                                              
+# 3.7 GB                                                      
+wget http://dgrp2.gnets.ncsu.edu/data/website/dgrp2.vcf
